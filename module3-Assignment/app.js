a4147020b736478c19f36c787bd54179f6b89d4a
NFKK <NFKK@NFKK-7490.in.oracle.com>
2021-02-14 01:55:58 +0530

    Module3_Assignment

--- module3-Assignment/app.js
+++ module3-Assignment/app.js
@@ -11,75 +11,32 @@
       var ddo = {
           templateUrl: 'foundItems.html',
           scope: {
-              menu: '<myList',
+              items: '<',
               onRemove: '&'
           },
 
-          controller: NarrowItDownDirectiveController,
+          controller: NarrowItDownController,
           controllerAs: 'menu',
-          bindToController: true,
-          link: NarrowItDownDirectiveLink
+          bindToController: true
       };
 
       return ddo;
   }
 
-  function NarrowItDownDirectiveLink(scope, element, attrs, controller) {
-      console.log("Link scope is: ", scope);
-      console.log("Controller instance is: ", controller);
-      console.log("Element is: ", element);
-
-      scope.$watch('menu.checkMenuLength()', function(newValue, oldValue) {
-          console.log("Old value: ", oldValue);
-          console.log("New value: ", newValue);
-
-          if (newValue === true) {
-              displayWarning();
-          } else {
-              removeWarning();
-          }
-
-      });
-
-      function displayWarning() {
-          var warningElem = element.find("div.error");
-          warningElem.slideDown(900);
-      }
-
-
-      function removeWarning() {
-          var warningElem = element.find("div.error");
-          warningElem.slideUp(900);
-      }
-  }
-
-  function NarrowItDownDirectiveController() {
-      var menu = this;
-
-      console.log('inside NarrowItDownDirectiveController');
-      menu.checkMenuLength = function() {
-
-          console.log('menu.menu.foundItems.length ' + menu.menu.foundItems.length);
-          if ( menu.menu.foundItems.length == 0) {
-              console.log('array length is zero');
-              return true;
-          } else {
-              console.log('array length is > 0');
-              return false;
-          }
-      };
-  }
-
   NarrowItDownController.$inject = ['MenuSearchService'];
+
   function NarrowItDownController(MenuSearchService) {
       var menu = this;
-      menu.foundItems = [];
 
-      menu.getMatchedMenuItems = function() {
-          
-          menu.foundItems = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
-          //menu.flag = 1;
-      }
+      menu.getMatchedMenuItems = function(searchTerm) {
+          MenuSearchService.getMatchedMenuItems(searchTerm).then(function(response) {
+                  menu.foundItems = response;
+              })
+              .catch(function(error) {
+                  console.log("error in getMatchedMenuItems");
+              });
+
+      };
 
       menu.removeItem = function(itemIndex) {
           console.log('inside remove ' + itemIndex);
@@ -89,32 +46,22 @@
       };
   }
 
+
   MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+
   function MenuSearchService($http, ApiBasePath) {
       var service = this;
       var foundItems = [];
 
       service.getMatchedMenuItems = function(searchTerm) {
-          foundItems = [];
-          var j = -1;
-
-          console.log('searchTerm is ' + searchTerm);
-          if (searchTerm == null || searchTerm == undefined || searchTerm == "") {
-              console.log('searchTerm is null');
-              return foundItems;
-          }
-
-          var response = $http({
-              method: "GET",
-              url: (ApiBasePath),
-          });
-
-          console.log('inside service' + response);
-          console.log(response.data);
-
-          var promise = response;
-          promise.then(function(response) {
-                  console.log(response.data); 
+          var promise = $http({
+                  method: "GET",
+                  url: (ApiBasePath),
+              });
+           return promise.then(function(response) {
+                  foundItems = [];
+                  var j = -1;
+                  console.log(response.data);
 
                   console.log(response.data.menu_items);
                   console.log(response.data.menu_items.length);
@@ -129,13 +76,11 @@
                       }
                   }
                   console.log('foundItems length ' + foundItems.length);
-
+                  return foundItems;
               })
               .catch(function(error) {
                   console.log(error);
               })
-
-          return foundItems;
       };
 
       service.removeItem = function(itemIndex) {
